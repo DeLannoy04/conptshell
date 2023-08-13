3c7d04420c364d8442cabf0d9d6375a8b30f05e5
DeLannoy04 <53373398+DeLannoy04@users.noreply.github.com>
2023-08-13 12:32:42 +0200

    a

--- Invoke-ConPtyShell.ps1
+++ Invoke-ConPtyShell.ps1
@@ -116,7 +116,7 @@ function Invoke-ConPtyShell
     $compilerOptions = New-Object System.CodeDom.Compiler.CompilerParameters
     $compilerOptions.GenerateExecutable = $false
     $compilerOptions.GenerateInMemory = $true
-    $compilerOptions.ReferencedAssemblies.Add([System.Net.Sockets.Socket].Assembly.Location)
+    $compilerOptions.ReferencedAssemblies.Add([System.Net.Sockets.Socket].Assembly.FullName)
 
     $provider = New-Object Microsoft.CSharp.CSharpCodeProvider
     $compilerResults = $provider.CompileAssemblyFromSource($compilerOptions, $Source)
