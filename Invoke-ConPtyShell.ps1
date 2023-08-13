da8ace4e73257e5f389f81ee63b05e4b323de4a8
DeLannoy04 <53373398+DeLannoy04@users.noreply.github.com>
2023-08-13 11:56:04 +0200

    a

--- Invoke-ConPtyShell.ps1
+++ Invoke-ConPtyShell.ps1
@@ -116,7 +116,7 @@ function Invoke-ConPtyShell
     $compilerOptions = New-Object System.CodeDom.Compiler.CompilerParameters
     $compilerOptions.GenerateExecutable = $false
     $compilerOptions.GenerateInMemory = $true
-    $compilerOptions.ReferencedAssemblies.Add("System.Net")
+    $compilerOptions.ReferencedAssemblies.Add("System.Net.dll")
 
     $provider = New-Object Microsoft.CSharp.CSharpCodeProvider
     $compilerResults = $provider.CompileAssemblyFromSource($compilerOptions, $Source)
